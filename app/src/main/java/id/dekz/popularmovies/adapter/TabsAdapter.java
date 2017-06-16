f85a16de8412c50ea2ecc0d38f9531503064b582
Adi Andrea <dekz.itfz@gmail.com>
2017-06-16 11:07:11 +0800

    create tabs adapter

---
+++ app/src/main/java/id/dekz/popularmovies/adapter/TabsAdapter.java
@@ -0,0 +1,42 @@
+package id.dekz.popularmovies.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by DEKZ on 6/16/2017.
+ */
+
+public class TabsAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment> fragments = new ArrayList<>();
+    private List<String> fragmentTitles = new ArrayList<>();
+
+    public TabsAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    public void addFragment(Fragment fragment, String title){
+        fragments.add(fragment);
+        fragmentTitles.add(title);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentTitles.get(position);
+    }
+}
