78d7bc64d3db355143f8ef5fb21569d32433b12b
Adi Andrea <dekz.itfz@gmail.com>
2017-06-16 11:24:59 +0800

    attach fragment to tabs

--- app/src/main/java/id/dekz/popularmovies/features/main/MainActivity.java
+++ app/src/main/java/id/dekz/popularmovies/features/main/MainActivity.java
@@ -1,18 +1,36 @@
 package id.dekz.popularmovies.features.main;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
+import butterknife.BindString;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import id.dekz.popularmovies.R;
+import id.dekz.popularmovies.adapter.TabsAdapter;
+import id.dekz.popularmovies.features.highrated.HighRatedFragment;
+import id.dekz.popularmovies.features.mostpopular.MostPopularFragment;
 
 public class MainActivity extends AppCompatActivity implements MainView {
 
+    @BindView(R.id.toolbar)Toolbar toolbar;
+    @BindView(R.id.container)ViewPager container;
+    @BindView(R.id.tabs)TabLayout tabLayout;
+    @BindString(R.string.most_popular)String mostPopularString;
+    @BindString(R.string.highest_rated)String highRatedString;
+
     private MainPresenter presenter;
+    private TabsAdapter tabsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
 
         onAttachView();
     }
@@ -28,10 +46,19 @@ public class MainActivity extends AppCompatActivity implements MainView {
         presenter = new MainPresenter();
         presenter.onAttach(this);
 
+        setupTabs();
     }
 
     @Override
     public void onDetachView() {
         presenter.onDetach();
+    }
+
+    private void setupTabs(){
+        tabsAdapter = new TabsAdapter(getSupportFragmentManager());
+        tabsAdapter.addFragment(new MostPopularFragment(), mostPopularString);
+        tabsAdapter.addFragment(new HighRatedFragment(), highRatedString);
+        container.setAdapter(tabsAdapter);
+        tabLayout.setupWithViewPager(container);
     }
 }
