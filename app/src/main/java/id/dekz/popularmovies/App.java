ca0fe631deb700072e1929a72126bad31e7d1417
Adi Andrea <dekz.itfz@gmail.com>
2017-06-16 10:18:40 +0800

    add app class

---
+++ app/src/main/java/id/dekz/popularmovies/App.java
@@ -0,0 +1,15 @@
+package id.dekz.popularmovies;
+
+import android.app.Application;
+
+/**
+ * Created by DEKZ on 6/16/2017.
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
