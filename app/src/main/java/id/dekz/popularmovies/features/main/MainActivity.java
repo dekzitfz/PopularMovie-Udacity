9dcf271fd9f44d3e758731f0e0db7f85f97058f6
Adi Andrea <dekz.itfz@gmail.com>
2017-06-17 10:34:03 +0800

    create menu in main

--- app/src/main/java/id/dekz/popularmovies/features/main/MainActivity.java
+++ app/src/main/java/id/dekz/popularmovies/features/main/MainActivity.java
@@ -1,33 +1,23 @@
 package id.dekz.popularmovies.features.main;
-
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import id.dekz.popularmovies.R;
-import id.dekz.popularmovies.adapter.TabsAdapter;
-import id.dekz.popularmovies.features.highrated.HighRatedFragment;
-import id.dekz.popularmovies.features.mostpopular.MostPopularFragment;
 
 public class MainActivity extends AppCompatActivity implements MainView {
 
     @BindView(R.id.toolbar)Toolbar toolbar;
-    @BindView(R.id.container)ViewPager container;
-    @BindView(R.id.tabs)TabLayout tabLayout;
-    @BindString(R.string.most_popular)String mostPopularString;
-    @BindString(R.string.highest_rated)String highRatedString;
+    @BindView(R.id.swipe_refresh)SwipeRefreshLayout swipeRefresh;
+    @BindView(R.id.rv)RecyclerView rv;
 
     private MainPresenter presenter;
-    private TabsAdapter tabsAdapter;
-
-    private MostPopularFragment mostPopularFragment;
-    private HighRatedFragment highRatedFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,31 +27,37 @@ public class MainActivity extends AppCompatActivity implements MainView {
         setSupportActionBar(toolbar);
 
         if (savedInstanceState != null) {
-            //Restore the fragment's instance
-            mostPopularFragment = (MostPopularFragment) getSupportFragmentManager()
-                    .getFragment(savedInstanceState, mostPopularString);
-            highRatedFragment = (HighRatedFragment) getSupportFragmentManager()
-                    .getFragment(savedInstanceState, highRatedString);
+
         }else{
-            mostPopularFragment = new MostPopularFragment();
-            highRatedFragment = new HighRatedFragment();
+
         }
 
         onAttachView();
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_high_rated:
+                //do something
+                return true;
+            case R.id.menu_most_popular:
+                //do
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-
-        //Save the fragment's instance
-        for(int i=0; i<tabsAdapter.getCount(); i++){
-            getSupportFragmentManager()
-                    .putFragment(
-                            outState,
-                            tabsAdapter.getPageTitle(i).toString(),
-                            tabsAdapter.getItem(i));
-        }
     }
 
     @Override
@@ -75,19 +71,10 @@ public class MainActivity extends AppCompatActivity implements MainView {
         presenter = new MainPresenter();
         presenter.onAttach(this);
 
-        setupTabs();
     }
 
     @Override
     public void onDetachView() {
         presenter.onDetach();
-    }
-
-    private void setupTabs(){
-        tabsAdapter = new TabsAdapter(getSupportFragmentManager());
-        tabsAdapter.addFragment(mostPopularFragment, mostPopularString);
-        tabsAdapter.addFragment(highRatedFragment, highRatedString);
-        container.setAdapter(tabsAdapter);
-        tabLayout.setupWithViewPager(container);
     }
 }
